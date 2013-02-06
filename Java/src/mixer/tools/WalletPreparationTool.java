d127d99ddaa41f9d3e00045813c21319d5df2bcf
Nick La Rooy <nicklarooy@hotmail.com>
2013-02-06 14:11:32 +0000

    RF

--- Java/src/mixer/tools/WalletPreparationTool.java
+++ Java/src/mixer/tools/WalletPreparationTool.java
@@ -2,6 +2,8 @@ package mixer.tools;
 
 import java.io.File;
 import java.math.BigInteger;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import com.google.bitcoin.core.BlockChain;
 import com.google.bitcoin.core.ECKey;
@@ -10,6 +12,7 @@ import com.google.bitcoin.core.PeerGroup;
 import com.google.bitcoin.core.Transaction;
 import com.google.bitcoin.core.Utils;
 import com.google.bitcoin.core.Wallet;
+import com.google.bitcoin.core.Wallet.SendRequest;
 import com.google.bitcoin.discovery.DnsDiscovery;
 import com.google.bitcoin.discovery.IrcDiscovery;
 import com.google.bitcoin.store.BlockStore;
@@ -23,14 +26,6 @@ public strictfp final class WalletPreparationTool {
 	}
 	
 	public static void main(String[] args) throws Exception {
-		
-		/*
-		 * 1. Get the amount required, load the wallet-
-		 * 2. Generate a new address-
-		 * 3. Add it to the wallet and save-
-		 * 4. Generate a new transaction from the wallet to the new address
-		 * 5. Sync
-		 */
 		
 		if (args.length != 3) {
 			
@@ -50,13 +45,17 @@ public strictfp final class WalletPreparationTool {
 		final BigInteger amount = Utils.toNanoCoins(0, Integer.parseInt(args[2]));
 		
 		System.out.println("Preparing " + walletFile.toString() + " for a " + amount.toString() + " nanocoin mix. ");
+		System.out.println();
 		
 		// Load the wallet
 		System.out.println("Loading the wallet... ");
 		
 		final Wallet wallet = Wallet.loadFromFile(walletFile);
 		
+        wallet.autosaveToFile(walletFile, 200, TimeUnit.MILLISECONDS, null);
+		
 		System.out.println("Wallet loaded. ");
+		System.out.println();
 		
 		// Load the blockchain
 		System.out.println("Loading the blockchain... ");
@@ -65,13 +64,15 @@ public strictfp final class WalletPreparationTool {
 		final BlockChain blockChain = new BlockChain(wallet.getNetworkParameters(), wallet, blockStore);
 		
 		System.out.println("Blockchain loaded. ");
+		System.out.println();
 		
 		// Connect to the network
-		System.out.println("Starting the peer... ");
+		System.out.println("Creating the peer... ");
 		
 		final PeerGroup peerGroup = new PeerGroup(wallet.getNetworkParameters(), blockChain);
 		
-        peerGroup.addWallet(wallet);
+		peerGroup.setUserAgent("WalletPreparationTool", "1.0");
+		peerGroup.addWallet(wallet);
         
         if (wallet.getNetworkParameters().equals(NetworkParameters.prodNet())) {
             
@@ -85,37 +86,59 @@ public strictfp final class WalletPreparationTool {
         	
             throw new RuntimeException("Unreachable.");
         }
-        
-		peerGroup.startAndWait();
-		peerGroup.setMinBroadcastConnections(1);
 		
 		System.out.println("Peer ready. ");
+		System.out.println();
 		
 		//Create a new key
 		final ECKey key = new ECKey();
 		
 		System.out.println("Created a new key: ");
 		System.out.println(key.toStringWithPrivate());
+		System.out.println();
 		
 		// Save it to the wallet
 		System.out.println("Adding the key to the wallet... ");
 		
 		wallet.addKey(key);
-		wallet.saveToFile(walletFile);
+		
+		System.out.println("Key added to wallet. ");
+		System.out.println();
 		
 		// Create the transaction
 		System.out.println("Creating the transaction... ");
 		
-		final Transaction transaction = wallet.createSend(key.toAddress(wallet.getNetworkParameters()), amount);
+		final Transaction tx = new Transaction(wallet.getNetworkParameters());
 		
-		System.out.println("Transaction ready. ");
+		tx.addOutput(amount, key);
 		
-		// Broadcasting
-		System.out.println("Broadcasting... ");
+		final SendRequest sendRequest = Wallet.SendRequest.forTx(tx);
 		
-		peerGroup.broadcastTransaction(transaction).get();
-		
-		System.out.println("Broadcast complete. ");
+		if (wallet.completeTx(sendRequest)) {
+			
+			System.out.println("Transaction ready. ");
+			
+			System.out.println("The transaction: ");
+			System.out.println(sendRequest.tx.toString());
+			System.out.println();
+			
+			// Broadcasting
+			System.out.println("Broadcasting... ");
+			
+	        peerGroup.addWallet(wallet);
+	        peerGroup.setMinBroadcastConnections(1);
+	        
+			peerGroup.startAndWait();
+			
+			peerGroup.broadcastTransaction(sendRequest.tx).get();
+			
+			System.out.println("Broadcasting complete");
+		}
+		else {
+			
+			System.out.println("Not enough coins! ");
+			System.out.println();
+		}
 		
 		// Pack up
 		System.out.println("Packing up... ");
@@ -126,6 +149,6 @@ public strictfp final class WalletPreparationTool {
         
         peerGroup.stopAndWait();
         
-        System.out.println("Success! ");
+        System.out.println("Done. ");
 	}
 }
